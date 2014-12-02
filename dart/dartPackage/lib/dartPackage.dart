6184e152464ff082d09342f4e4f989cd2b27fce9
Andrew Malcolmson <andmalc@gmail.com>
2014-12-01 20:12:32 -0500

    dart tutorial and exercises

---
+++ dart/dartPackage/lib/dartPackage.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2014, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The dartPackage library.
+///
+/// This is an awesome library. More dartdocs go here.
+library dartPackage;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/dartPackage_base.dart';
